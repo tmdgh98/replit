b620c4fb03eaff77c84bfffa8d565f0789cecd04
tmdgh98 <duqrlwjstk@naver.com>
2021-01-11 10:28:04 +0000

    21/1/11

--- ItsCote/Unit12 구현문제/05a뱀.py
+++ ItsCote/Unit12 구현문제/05a뱀.py
@@ -36,3 +36,26 @@ def simulate():
     nx = x + dx[direction]
     ny = y + dy[direction]
     #맵 범위 안에 있고, 뱀의 몸통이 없는 위치라면
+    if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2:
+      #사과가 없다면 이동 후에 꼬리 제거
+      if data[nx][ny] == 0:
+        data[nx][ny] = 2
+        q.append((nx,ny))
+        px, py = q.pop(0)
+        data[px][py] = 0
+      #사과가 있다면 이동 후에 꼬리 그대로 두기
+      if data[nx][ny] == 1:
+        data[nx][ny] = 2
+        q.append((nx,ny))
+    #벽이나 몸통에 부딪혔다면
+    else :
+      time += 1
+      break
+    x, y = nx, ny # 다음 위치로 머리를 이동
+    time += 1
+    if index < 1 and time == info[index][0] : #회전할 시간인 경우 회전
+      direction = turn(direction, info[index][1])
+      index += 1
+  return time
+
+print(simulate())
